dcc25f0e69009b04f6f3ac60e74d3992a03be193
r-tamura <rtamura30@gmail.com>
2024-12-27 16:43:10 +0900

    ch3: HTTP GET

--- saba_core/src/http.rs
+++ saba_core/src/http.rs
@@ -27,7 +27,7 @@ pub struct HttpResponse {
 }
 
 impl HttpResponse {
-    fn new(raw_response: String) -> Result<Self, Error> {
+    pub fn new(raw_response: String) -> Result<Self, Error> {
         let preprocessed_response = raw_response.trim_start().replace("\n\r", "\n");
 
         let (status, remaining) = match preprocessed_response.split_once('\n') {
