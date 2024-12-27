30dd0f5146c778c5978df3ec1f24477a1671eb41
r-tamura <rtamura30@gmail.com>
2024-12-27 12:34:50 +0900

    ch2: URLパース
    - #[cfg(test)] は コードにメタデータを設定する仕組み Attributes の一つ

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -1,4 +1,5 @@
 use alloc::string::{String, ToString};
+use alloc::vec::Vec;
 
 #[derive(Debug, Clone, PartialEq)]
 struct Url {
@@ -11,12 +12,198 @@ struct Url {
 
 impl Url {
     pub fn new(url: String) -> Self {
-        return Self {
+        Self {
             url,
             host: "".to_string(),
             port: "".to_string(),
             path: "".to_string(),
             searchpart: "".to_string(),
-        };
+        }
+    }
+
+    pub fn host(&self) -> String {
+        self.host.clone()
+    }
+
+    pub fn port(&self) -> String {
+        self.port.clone()
+    }
+
+    pub fn path(&self) -> String {
+        self.path.clone()
+    }
+
+    pub fn searchpart(&self) -> String {
+        self.searchpart.clone()
+    }
+
+    fn is_http(&self) -> bool {
+        self.url.contains("http://")
+    }
+
+    fn extract_host(&self) -> String {
+        // "http://example.com:80/path"
+        //         -----------
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        assert!(url_parts.len() > 0, "Invalid URL");
+        if let Some(index) = url_parts[0].find(':') {
+            url_parts[0][..index].to_string()
+        } else {
+            url_parts[0].to_string()
+        }
+    }
+
+    fn extract_port(&self) -> String {
+        // "http://example.com:80/path"
+        //                     --
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+
+        assert!(url_parts.len() > 0, "Invalid URL");
+        if let Some(index) = url_parts[0].find(":") {
+            url_parts[0][index + 1..].to_string()
+        } else {
+            "80".to_string()
+        }
+    }
+
+    fn extract_path(&self) -> String {
+        // "http://example.com:80/path?key=valeu1#fragment"
+        //                        ----
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+        if url_parts.len() < 2 {
+            return "".to_string();
+        }
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, "?").collect();
+        assert!(path_and_searchpart.len() > 0, "Invalid URL");
+        path_and_searchpart[0].to_string()
+    }
+
+    fn extract_searchpart(&self) -> String {
+        // "http://example.com:80/path?key=valeu1#fragment"
+        //                             ----------
+        let url_parts: Vec<&str> = self
+            .url
+            .trim_start_matches("http://")
+            .splitn(2, "/")
+            .collect();
+        if url_parts.len() < 2 {
+            return "".to_string();
+        }
+        let path_and_searchpart: Vec<&str> = url_parts[1].splitn(2, "?").collect();
+        assert!(path_and_searchpart.len() > 0, "Invalid URL");
+        if path_and_searchpart.len() < 2 {
+            "".to_string()
+        } else {
+            path_and_searchpart[1].to_string()
+        }
+    }
+
+    pub fn parse(&mut self) -> Result<Self, String> {
+        if !self.is_http() {
+            return Err("Only HTTP scheme is supported.".to_string());
+        }
+        self.host = self.extract_host();
+        self.port = self.extract_port();
+        self.path = self.extract_path();
+        self.searchpart = self.extract_searchpart();
+
+        Ok(self.clone())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_url_host() {
+        let url = "http://example.com".to_string();
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "80".to_string(),
+            path: "".to_string(),
+            searchpart: "".to_string(),
+        });
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    #[test]
+    fn test_url_host_port() {
+        let url = "http://example.com:8888".to_string();
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "8888".to_string(),
+            path: "".to_string(),
+            searchpart: "".to_string(),
+        });
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    #[test]
+    fn test_url_host_port_path() {
+        let url = "http://example.com:8888/index.html".to_string();
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "8888".to_string(),
+            path: "index.html".to_string(),
+            searchpart: "".to_string(),
+        });
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    #[test]
+    fn test_url_host_path() {
+        let url = "http://example.com/index.html".to_string();
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "80".to_string(),
+            path: "index.html".to_string(),
+            searchpart: "".to_string(),
+        });
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    #[test]
+    fn test_url_host_port_path_searchpart() {
+        let url = "http://example.com:8888/index.html?a=123&b=456".to_string();
+        let expected = Ok(Url {
+            url: url.clone(),
+            host: "example.com".to_string(),
+            port: "8888".to_string(),
+            path: "index.html".to_string(),
+            searchpart: "a=123&b=456".to_string(),
+        });
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    #[test]
+    fn test_no_scheme() {
+        let url = "example.com".to_string();
+        let expected = Err("Only HTTP scheme is supported.".to_string());
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    #[test]
+    fn test_unsupported_scheme() {
+        let url = "https://example.com:8888/index.html".to_string();
+        let expected = Err("Only HTTP scheme is supported.".to_string());
+        assert_eq!(expected, Url::new(url).parse());
     }
 }
